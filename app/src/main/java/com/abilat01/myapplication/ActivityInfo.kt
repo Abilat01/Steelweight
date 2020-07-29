1b92dd4a054fdd32221d8ec251ed3bad4090dfb7
Abilat01 <abilat01@yandex.ru>
2020-07-29 16:37:46 +0300

    UA new + cod

---
+++ app/src/main/java/com/abilat01/myapplication/ActivityInfo.kt
@@ -0,0 +1,11 @@
+package com.abilat01.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info)
+    }
+}
--- app/src/main/java/com/abilat01/myapplication/MainActivity.kt
+++ app/src/main/java/com/abilat01/myapplication/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.abilat01.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener { //наследуемся
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        nav_view.setNavigationItemSelectedListener (this) //привязка к конкретному RecyclerView
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.id_raz -> Toast.makeText(this, "Разминка", Toast.LENGTH_LONG).show()
+            R.id.id_program -> Toast.makeText(this, "Программы", Toast.LENGTH_LONG).show()
+            R.id.id_exercises -> Toast.makeText(this, "Упражнения", Toast.LENGTH_LONG).show()
+            R.id.id_info -> intentInfoActivity()
+            R.id.id_exit -> exitMenu() //первый из 5 готов)
+        }
+
+        return true
+    }
+
+    fun exitMenu(){
+        finish()
+    }
+
+    fun intentInfoActivity(){
+        val intentGo = Intent(this, ActivityInfo::class.java)
+        startActivity(intentGo)
+    }
+
 }
